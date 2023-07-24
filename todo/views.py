e0abb147982ab63dc535e74ebef8b143013476b8
Camelia03 <>
2023-07-24 14:00:28 +0000

    Creating a new task part 1 and 2

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 
 # Create your views here.
@@ -10,3 +10,13 @@ def get_todo_list(request):
         'tasks': tasks
     }
     return render(request, 'todo/todo_list.html', context)
+
+
+def add_task(request):
+    if request.method == 'POST':
+        name = request.POST.get('task_name')
+        done = 'done' in request.POST
+        Task.objects.create(name=name, done=done)
+
+        return redirect('get_todo_list')
+    return render(request, 'todo/add_task.html')
